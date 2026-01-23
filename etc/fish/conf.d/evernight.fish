44f155e78766d268bc9bed28e55bdbc34aa72147
长夜月玩Fedora <13278297951@sina.cn>
2026-01-23 18:56:29 +0800

    Update Fish Greet.

--- etc/fish/conf.d/evernight.fish
+++ etc/fish/conf.d/evernight.fish
@@ -1,6 +1,19 @@
+# Copyright (C) 2006-2026 EvernightFedora All rights reserved.
 function fish_greeting
-    echo "Evernight Vista [Version:43.1]"
-    echo "(C) 2026 EvernightFedora All rights reserved."
+    set codename (grep -E '^VERSION_CODENAME=' /usr/lib/os-release 2>/dev/null | cut -d '=' -f 2 | tr -d '"')
+    if test -z "$codename"
+        set codename "unknown"
+    end
+    set sysver (grep -E '^VERSION=' /usr/lib/os-release 2>/dev/null | cut -d '=' -f 2 | tr -d '"')
+    if test -z "$sysver"
+        set sysver "unknown"
+    end
+    set arch (uname -m)
+    if test -z "$arch"
+        set arch "unknown"
+    end
+    echo "Evernight Vista $sysver ($codename) $arch"
+    echo "(C) 2006-2026 EvernightFedora All rights reserved."
 end
 alias cls="clear"
 alias ls="lsd"
