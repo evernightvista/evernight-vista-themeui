a9b3a267162cb4a8808af508e34aa5964f0752f4
长夜月玩Fedora <13278297951@sina.cn>
2026-02-05 11:33:28 +0800

    Added Fish Greeting Root Detect Features.

--- etc/fish/conf.d/evernight.fish
+++ etc/fish/conf.d/evernight.fish
@@ -4,15 +4,28 @@ function fish_greeting
     if test -z "$codename"
         set codename "unknown"
     end
+
     set sysver (grep -E '^VERSION=' /usr/lib/os-release 2>/dev/null | cut -d '=' -f 2 | tr -d '"')
     if test -z "$sysver"
         set sysver "unknown"
     end
+
     set arch (uname -m)
     if test -z "$arch"
         set arch "unknown"
     end
-    echo "Evernight Vista $sysver ($codename) $arch"
+
+    set is_root (whoami | grep -c '^root$')
+
+    set red (set_color red)
+    set reset (set_color normal)
+
+    set greeting "Evernight Vista $sysver ($codename) $arch"
+    if test $is_root -eq 1
+        set greeting "$greeting $red(as root)$reset"
+    end
+
+    echo $greeting
     echo "(C) 2006-2026 KairikiFedora All rights reserved."
 end
 alias cls="clear"
